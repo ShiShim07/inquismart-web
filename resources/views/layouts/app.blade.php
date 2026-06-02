c3a273e7ed21cf62c565486d417ef24e2a74eebb
Shishim07 <sborlagdan17@gmail.com>
2026-06-02 21:33:25 +0800

    update inquibot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'InquiSmart Admin')</title>
+    <title>@yield('title', 'InquiBot Admin')</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4/dist/full.min.css" rel="stylesheet" type="text/css" />
     @vite(['resources/css/app.css'])
